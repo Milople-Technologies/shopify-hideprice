0f6ae5539b3f5e45965063c489f4aa64c9bd7825
akilmilople <akil@milople.com>
2025-04-03 11:39:42 +0000

    pop

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,30 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
-import { AppProvider } from "@shopify/polaris";
-import "@shopify/polaris/build/esm/styles.css";
-import { Provider, useAppBridge } from "@shopify/app-bridge-react";
-import createApp from "@shopify/app-bridge";
-import { getSession } from "./session.server"; // This should handle Shopify session data
-import { authenticatedFetch } from "@shopify/app-bridge/utilities";
-import { Redirect } from "@shopify/app-bridge/actions";
-
-export async function loader({ request }) {
-  const session = await getSession(request.headers.get("Cookie"));
-  return { shop: session.shop, apiKey: process.env.SHOPIFY_API_KEY };
-}
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
 export default function App() {
-  const { shop, apiKey } = useLoaderData();
-
-  const config = {
-    apiKey,
-    shopOrigin: shop,
-    forceRedirect: true,
-  };
-
   return (
-    <Provider config={config}>
-      <AppProvider>
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-      </AppProvider>
-    </Provider>
+      </body>
+    </html>
   );
 }
