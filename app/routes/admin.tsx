d85d6423084567b34b0d5adb1afcbf06a45e4232
akilmilople <akil@milople.com>
2025-04-03 07:19:17 +0000

    dev0

---
+++ app/routes/admin.tsx
@@ -0,0 +1,38 @@
+import { Page, Layout, Card, FormLayout, TextField, Select, Button } from "@shopify/polaris";
+import { useState } from "react";
+
+export default function AdminSettings() {
+  const [textValue, setTextValue] = useState("");
+  const [dropdownValue, setDropdownValue] = useState("option1");
+
+  const handleSubmit = async () => {
+    console.log("Saving Data:", { textValue, dropdownValue });
+    // TODO: Add API request to save settings
+  };
+
+  return (
+    <Page title="Hide Price Settings">
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <FormLayout>
+              <TextField label="Custom Text" value={textValue} onChange={setTextValue} />
+              <Select
+                label="Select Option"
+                options={[
+                  { label: "Option 1", value: "option1" },
+                  { label: "Option 2", value: "option2" },
+                ]}
+                value={dropdownValue}
+                onChange={setDropdownValue}
+              />
+              <Button primary onClick={handleSubmit}>
+                Save Settings
+              </Button>
+            </FormLayout>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
